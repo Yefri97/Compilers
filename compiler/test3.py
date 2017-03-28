db2758fbf045847c02d9adc12f5aaad62debb536
Catalina Castro <catalina.cst@hotmail.es>
2017-03-28 09:22:55 -0500

    Test especial para errores

--- compiler/test3.py
+++ compiler/test3.py
@@ -1,6 +1,14 @@
 MODULE Adder;
-CONST N := 8;
+CONST N := 0;
+IN x, y: [N] BIT; ci: BIT;
 OUT s: [N] BIT; co: BIT;
-VAR c: [N] BIT;
 BEGIN
+  S.0(X.0, Y.0, ci); (* inputs in cell 0*)
+  FOR o:=1 N-1 DO
+    S.i(X.i,Y.i,S[i-1].co) (* inputs in cell i *)
+  END;
+  FOR i 0..N-1 DO
+    Z.i:=S.i.z
+  END;
+  co:=S.7.co
 END Adder.
