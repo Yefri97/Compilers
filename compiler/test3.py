ca03e015f34b7170084c21621d08f850bbb02221
Catalina Castro <catalina.cst@hotmail.es>
2017-03-28 09:51:13 -0500

    Un test 3 para la prueba de errores

--- compiler/test3.py
+++ compiler/test3.py
@@ -1,13 +1,13 @@
 MODULE Adder;
-CONST N := 0;
-IN x, y: [N] BIT; ci: BIT;
+CONST N  0;
+IN x, y: [N] BIT; ci: BIT
 OUT s: [N] BIT; co: BIT;
 BEGIN
   S.0(X.0, Y.0, ci); (* inputs in cell 0*)
-  FOR o:=1 N-1 DO
+  FOR o:=1  N-1 DO
     S.i(X.i,Y.i,S[i-1].co) (* inputs in cell i *)
   END;
-  FOR i 0..N-1 DO
+  FOR i := 0..N-1 DO
     Z.i:=S.i.z
   END;
   co:=S.7.co
